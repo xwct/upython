10097df41e021fade45a449a74f745cbf376b273
xwct <pettersson.ole@gmail.com>
2020-06-25 21:18:40 +0200

    dir cleanup

--- pyboard/upython/ubit/main.py
+++
@@ -1,9 +0,0 @@
-from microbit import *
-
-def f_up():
-    while True:
-        gesture = accelerometer.current_gesture()
-        if gesture == "face up":
-            display.show(Image.HAPPY)
-        else:
-            display.show(Image.SAD)
